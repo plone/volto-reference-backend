8d987d1a6e6045832ef552a05c988aed192e9441
Rob Gietema <rob.gietema@gmail.com>
2022-02-27 11:36:26 +0100

    Fix initial content.

--- seeds/001_document.js
+++ seeds/001_document.js
@@ -13,10 +13,33 @@ exports.seed = (knex) =>
             title: 'Welcome to Volto',
             description:
               'Congratulations! You have successfully installed Volto.',
-            layout: 'document_view',
-            text: {
-              'content-type': 'text/html',
-              data: "If you're seeing this instead of the web site you were expecting, the owner of this web site has just installed Volto. Do not contact the Volto Team or the Volto support channels about this.",
+            blocks: {
+              '79ba8858-1dd3-4719-b731-5951e32fbf79': {
+                '@type': 'title',
+              },
+              '495efb73-cbdd-4bef-935a-a56f70a20854': {
+                '@type': 'text',
+                text: {
+                  blocks: [
+                    {
+                      key: '9f35d',
+                      text: "If you're seeing this instead of the web site you were expecting, the owner of this web site has just installed Volto. Do not contact the Volto Team or the Volto support channels about this.",
+                      type: 'unstyled',
+                      depth: 0,
+                      inlineStyleRanges: [],
+                      entityRanges: [],
+                      data: {},
+                    },
+                  ],
+                  entityMap: {},
+                },
+              },
+            },
+            blocks_layout: {
+              items: [
+                '79ba8858-1dd3-4719-b731-5951e32fbf79',
+                '495efb73-cbdd-4bef-935a-a56f70a20854',
+              ],
             },
           },
         },
@@ -29,7 +52,23 @@ exports.seed = (knex) =>
           position_in_parent: 0,
           json: {
             title: 'News',
-            layout: 'listing_view',
+            blocks: {
+              '79ba8858-1dd3-4719-b731-5951e32fbf79': {
+                '@type': 'title',
+              },
+              'e3d1a578-d467-4666-9781-0cc9eeed31ed': {
+                '@type': 'listing',
+                query: [],
+                block: 'e3d1a578-d467-4666-9781-0cc9eeed31ed',
+                variation: 'default',
+              },
+            },
+            blocks_layout: {
+              items: [
+                '79ba8858-1dd3-4719-b731-5951e32fbf79',
+                'e3d1a578-d467-4666-9781-0cc9eeed31ed',
+              ],
+            },
           },
         },
         {
@@ -41,7 +80,23 @@ exports.seed = (knex) =>
           position_in_parent: 1,
           json: {
             title: 'Events',
-            layout: 'listing_view',
+            blocks: {
+              '79ba8858-1dd3-4719-b731-5951e32fbf79': {
+                '@type': 'title',
+              },
+              'e3d1a578-d467-4666-9781-0cc9eeed31ed': {
+                '@type': 'listing',
+                query: [],
+                block: 'e3d1a578-d467-4666-9781-0cc9eeed31ed',
+                variation: 'default',
+              },
+            },
+            blocks_layout: {
+              items: [
+                '79ba8858-1dd3-4719-b731-5951e32fbf79',
+                'e3d1a578-d467-4666-9781-0cc9eeed31ed',
+              ],
+            },
           },
         },
         {
@@ -53,7 +108,23 @@ exports.seed = (knex) =>
           position_in_parent: 2,
           json: {
             title: 'Users',
-            layout: 'listing_view',
+            blocks: {
+              '79ba8858-1dd3-4719-b731-5951e32fbf79': {
+                '@type': 'title',
+              },
+              'e3d1a578-d467-4666-9781-0cc9eeed31ed': {
+                '@type': 'listing',
+                query: [],
+                block: 'e3d1a578-d467-4666-9781-0cc9eeed31ed',
+                variation: 'default',
+              },
+            },
+            blocks_layout: {
+              items: [
+                '79ba8858-1dd3-4719-b731-5951e32fbf79',
+                'e3d1a578-d467-4666-9781-0cc9eeed31ed',
+              ],
+            },
           },
         },
       ]),
